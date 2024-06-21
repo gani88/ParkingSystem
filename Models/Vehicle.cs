ee5136a1f0baa525ea03d2c31e71258c30f0057b
zarekgema8 <zarekgema@gmail.com>
2024-06-21 20:04:29 +0700

    [ADD] Add models vehicle

---
+++ Models/Vehicle.cs
@@ -0,0 +1,17 @@
+namespace ParkingSystem.Models 
+
+{
+    // Class for create entity Vehicle (Car & Motorcycle)
+    public class Vehicle {
+        public string Type { get; set; }
+        public string VehicleColor { get; set; }
+        public string PlateNumber { get; set; }
+
+            // Constructor for this class
+        public Vehicle(string type, string vehicleColor, string plateNumebr) {
+            this.Type = type;
+            this.VehicleColor = vehicleColor;
+            this.PlateNumber = plateNumebr;
+        }
+    }
+}
